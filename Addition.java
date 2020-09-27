eeec818cd720b1942dfe333585e4569b4121642b
Amulya54495 <70424596+Amulya54495@users.noreply.github.com>
2020-09-27 15:41:43 +0530

    Add files via upload
    //This is addition program

---
+++ Addition.java
@@ -0,0 +1,7 @@
+public class Addition {
+    public static void main(String[] args){
+        int a=20,b=10,c;
+         c=a+b;
+         System.out.println("addition of two numbers is:" +c);
+    }
+}
